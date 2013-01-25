053a9c006da4d3d7a1f0e9f530c0ea966cc17b82
Jt Gleason <jt@justin.tv>
2013-01-25 15:49:18 -0800

    tests pass

--- lib/activerecord-postgres-hstore-core.rb
+++ lib/activerecord-postgres-hstore-core.rb
@@ -3,6 +3,7 @@ require "active_record"
 
 require "activerecord_postgres_hstore_core/string"
 require "activerecord_postgres_hstore_core/hash"
+require "activerecord_postgres_hstore_core/coder"
 
 require "activerecord_postgres_hstore_core/activerecord"
 
